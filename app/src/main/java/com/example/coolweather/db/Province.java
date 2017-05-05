e1cd56802b10ac18af7573af2f5925c155d9f9e5
lvxuu <157483267@qq.com>
2017-05-05 21:49:30 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,44 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ${lvx} on 2017/5/5.
+ * com.example.coolweather.db
+ * CoolWeather
+ * 省份类
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private  String provinceName;
+    private int provinceCode;
+
+    /**
+     * alt +insert
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
